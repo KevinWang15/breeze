5987c141071872c33dd5cb5a7ccfd8d5a47ca272
Ke Wang <453587854@qq.com>
2020-05-16 23:35:02 +0800

    frontend: use chrome.storage where applicable

--- src/api/index.js
+++ src/api/index.js
@@ -1,39 +1,71 @@
 import axios from "axios";
+import 'babel-polyfill';
 
-function getUser() {
-    if (localStorage["breezeUser"]) {
-        return localStorage["breezeUser"];
-    }
-    localStorage["breezeUser"] = prompt("Breeze Browser Extension: which user would you like to log in as?", "user");
-    return getUser();
+function getStorage(key) {
+    return new Promise((resolve, reject) => {
+        if (chrome && chrome.storage) {
+            chrome.storage.sync.get([key], function (result) {
+                resolve(result[key])
+            });
+        } else {
+            resolve(localStorage[key]);
+        }
+    });
 }
 
-let user = getUser();
-const headers = {
-    Authentication: JSON.stringify({"type": "noauth", user})
-};
+function setStorage(key, value) {
+    return new Promise((resolve, reject) => {
+        if (chrome && chrome.storage) {
+            chrome.storage.sync.set({[key]: value}, function () {
+                resolve();
+            });
+        } else {
+            localStorage[key] = value;
+            resolve();
+        }
+    });
+}
 
+async function getUser() {
+    const storageKey = "breezeUser";
+
+    const currentValue = await getStorage(storageKey);
+    if (currentValue) {
+        return currentValue;
+    }
+    const user = prompt("Breeze Browser Extension: which user would you like to log in as?", "user");
+    await setStorage(storageKey, user);
+    alert("Breeze Browser Extension: user name set");
+    return user;
+}
+
+let user;
+getUser().then(value => user = value);
+
+const getHeaders = () => getUser().then(user => ({
+    Authentication: JSON.stringify({"type": "noauth", user})
+}));
 
 function getUrl(path) {
     return `http://127.0.0.1:3980/${path}`;
 }
 
 const saveAnnotation = ({url, uid, data}) => {
-    return axios.post(getUrl("saveAnnotation"), {
+    return getHeaders().then(headers => axios.post(getUrl("saveAnnotation"), {
         url, uid, data
-    }, {headers: headers});
+    }, {headers: headers}));
 };
 
 const deleteAnnotation = ({url, uid}) => {
-    return axios.post(getUrl("deleteAnnotation"), {
+    return getHeaders().then(headers => axios.post(getUrl("deleteAnnotation"), {
         url, uid,
-    }, {headers: headers});
+    }, {headers: headers}));
 };
 
 const getAnnotationsByUrl = (url) => {
-    return axios.post(getUrl("getAnnotationsByUrl"), {
+    return getHeaders().then(headers => axios.post(getUrl("getAnnotationsByUrl"), {
         url,
-    }, {headers: headers}).then(item => item.data.result);
+    }, {headers: headers}).then(item => item.data.result));
 };
 
 export {saveAnnotation, deleteAnnotation, getAnnotationsByUrl};
